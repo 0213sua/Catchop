94703d74abd8cc5f50dcbdd9cc93ab8226efbb2e
YOJU-e <red23457039@gmail.com>
2022-11-08 20:30:29 +0900

    for viewBinding

--- app/src/main/java/com/example/mp_teamproject/Main.kt
+++ app/src/main/java/com/example/mp_teamproject/Main.kt
@@ -3,6 +3,7 @@ package com.example.mp_teamproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.mp_teamproject.databinding.ActivityMainBinding
 
 class Main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
