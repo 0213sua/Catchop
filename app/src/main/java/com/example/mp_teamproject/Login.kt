f8518384a77da9513a5f7b6e1cdc610c5074961a
0213sua <0213tndk@gmail.com>
2022-11-10 22:21:02 +0900

    binding 1

--- app/src/main/java/com/example/mp_teamproject/Login.kt
+++ app/src/main/java/com/example/mp_teamproject/Login.kt
@@ -1,11 +1,25 @@
 package com.example.mp_teamproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mp_teamproject.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_login)
+
+        binding.Login.setOnClickListener{
+            val intent = Intent(this,Main::class.java)
+            startActivity(intent)
+        }
+
+        binding.SignUp.setOnClickListener{
+            val intent = Intent(this,SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
