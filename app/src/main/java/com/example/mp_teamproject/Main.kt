9e5c63850f13e19c9ab06e44554a9c074fdf5e0b
YOJU-e <red23457039@gmail.com>
2022-11-11 11:36:29 +0900

    make navigation bar with 4 fragments

--- app/src/main/java/com/example/mp_teamproject/Main.kt
+++ app/src/main/java/com/example/mp_teamproject/Main.kt
@@ -2,17 +2,40 @@ package com.example.mp_teamproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.fragment.app.Fragment
 import com.example.mp_teamproject.databinding.ActivityMainBinding
 
 class Main : AppCompatActivity() {
+    val homeFragment = HomeFragment()
+    val mytreeFragment = MytreeFragment()
+    val categoryFragment = CategoryFragment()
+    val myPageFragment = MypageFragment()
+    private val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        //val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.navigationview.setOnItemSelectedListener {
+            //change fragment
+                MenuItem -> when(MenuItem.itemId){
+            R.id.home_menu -> changeFragment(homeFragment)
+            R.id.mytree_menu -> changeFragment(mytreeFragment)
+            R.id.category_menu -> changeFragment(categoryFragment)
+            R.id.mypage_menu -> changeFragment(myPageFragment)
+        }
+            true
+        }
+        //init fragment
 
 
     }
+    private fun changeFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.fragments_container,fragment)
+            commit()
+        }
+    }
+
 }
