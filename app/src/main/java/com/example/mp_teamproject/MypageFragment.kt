4338cc347cc5994b83a95fa5abcaae23a7314fe3
YOJU-e <red23457039@gmail.com>
2022-11-11 11:42:11 +0900

    mypage fragment

---
+++ app/src/main/java/com/example/mp_teamproject/MypageFragment.kt
@@ -0,0 +1,33 @@
+package com.example.mp_teamproject
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MypageFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mypage, container, false)
+    }
+
+    companion object {
+        fun newInstance():MypageFragment{
+            return MypageFragment()
+        }
+    }
+}
