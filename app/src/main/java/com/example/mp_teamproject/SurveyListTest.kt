60aa372843a42a65a8af355ee28bf35033c66325
thoongee <cjina1102@gmail.com>
2022-11-16 00:02:01 +0900

    Make SurveyListTest.kt

---
+++ app/src/main/java/com/example/mp_teamproject/SurveyListTest.kt
@@ -0,0 +1,11 @@
+package com.example.mp_teamproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SurveyListTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_survey_list_test)
+    }
+}
