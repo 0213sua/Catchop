15d732604ddb693308ce4d13acc0e381af7cc457
0213sua <0213tndk@gmail.com>
2022-11-16 15:25:12 +0900

    sign up

--- app/src/main/java/com/example/mp_teamproject/SignUp.kt
+++ app/src/main/java/com/example/mp_teamproject/SignUp.kt
@@ -1,16 +1,16 @@
 package com.example.mp_teamproject
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.telephony.PhoneNumberFormattingTextWatcher
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
-import com.example.mp_teamproject.databinding.ActivityLoginBinding
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mp_teamproject.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-
 
 
 class SignUp : AppCompatActivity() {
@@ -25,9 +25,35 @@ class SignUp : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
 
         binding.SUSignupBtn.setOnClickListener {
-            createAccount(binding.SignupID.text.toString().trim(),binding.SignupPassword.text.toString().trim())
-       }
+            createAccount(
+                binding.SignupID.text.toString().trim(),
+                binding.SUPwcheckEditText.text.toString().trim()
+            )
+        }
+
+        binding.SUPhoneEditText.addTextChangedListener(PhoneNumberFormattingTextWatcher())
+
+        binding.SUPwcheckEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if (binding.SUPwEditText.getText().toString().trim()
+                        .equals(binding.SUPwcheckEditText.getText().toString().trim())
+                ) {
+                    binding.SUPwchecktextText.setText("Password matches ! :)")
+                    // 가입하기 버튼 활성화
+                    binding.SUSignupBtn.isEnabled = true
+                } else {
+                    binding.SUPwchecktextText.setText("Password doesn't match .. :( ")
+                    // 가입하기 버튼 비활성화
+                    binding.SUSignupBtn.isEnabled = false
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+        })
     }
+
 
     private fun createAccount(email: String, password: String) {
         auth.createUserWithEmailAndPassword(email,password)
@@ -45,5 +71,6 @@ class SignUp : AppCompatActivity() {
                 Toast.makeText(this,"회원가입 실패", Toast.LENGTH_SHORT).show()
             }
     }
+
 
 }
