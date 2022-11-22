f4cf2aef02a064b109260b8775db0b5b836355f2
thoongee <cjina1102@gmail.com>
2022-11-23 01:09:03 +0900

    SurveyInfo.kt : parti, statistic btn 각각 해당 uri로 연결되게 수정 CreateSurvey.kt : +add 버튼 누르면 구글폼 생성 사이트로 이동

--- app/src/main/java/com/example/mp_teamproject/SurveyInfo.kt
+++ app/src/main/java/com/example/mp_teamproject/SurveyInfo.kt
@@ -1,5 +1,7 @@
 package com.example.mp_teamproject
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -16,14 +18,14 @@ import kotlinx.android.synthetic.main.survey_post.view.*
 
 class SurveyInfo : AppCompatActivity() {
     val binding by lazy{ ActivitySurveyInfoBinding.inflate(layoutInflater)}
+    var partiUri =""
+    var staticUri = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        //participate btn
 
-        //statistic btn
 
         val surveyId = intent.getStringExtra("surveyId")
         Log.d("ITM","1")
@@ -34,32 +36,6 @@ class SurveyInfo : AppCompatActivity() {
                 override fun onCancelled(error: DatabaseError) {
                 }
                 // data 읽기
-                /*
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    val survey = dataSnapshot.getValue(SurveyData::class.java)
-                    if (survey != null) {
-                        si_titleText.setText(survey.title)
-                    }
-                    if (survey != null) {
-                        si_instText.setText(survey.institution)
-                    }
-                    if (survey != null) {
-                        si_sdateText.setText(survey.startDate)
-                    }
-                    if (survey != null) {
-                        si_edateText.setText(survey.endDate)
-                    }
-                    if (survey != null) {
-                        si_purposeText.setText(survey.purpose)
-                    }
-                    if (survey != null) {
-                        si_contentText.setText(survey.surveyContent)
-                    }
-                }
-
-                 */
-
-
                 override fun onDataChange(snapshot: DataSnapshot) {
                     Log.d("ITM","2")
                     snapshot?.let{
@@ -74,11 +50,28 @@ class SurveyInfo : AppCompatActivity() {
                             binding.siEdateText.text = survey.endDate
                             binding.siPurposeText.text = survey.purpose
                             binding.siContentText.text = survey.surveyContent
-                            Log.d("ITM","5")
-
+                            partiUri = survey.uri
+                            var strList: List<String> = survey.uri.split("/")
+                            Log.d("ITM","strList : $strList")
+                            staticUri = "https://docs.google.com/forms/d/e/"+strList[6]+"/viewanalytics"
+                            Log.d("ITM","$staticUri")
                         }
                     }
                 }
             })
+        //participate btn
+        binding.siPartiBtn.setOnClickListener {
+            // save implict intent(ACTION_VIEW) & pass uri string(github address)
+            val parti = Intent(Intent.ACTION_VIEW, Uri.parse("$partiUri"))
+            startActivity(parti)
+        }
+
+        //statistic btn
+
+        binding.siStaticBtn.setOnClickListener {
+            // save implict intent(ACTION_VIEW) & pass uri string(github address)
+            val static = Intent(Intent.ACTION_VIEW, Uri.parse("$staticUri"))
+            startActivity(static)
+        }
     }
 }
